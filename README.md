343c63c8e6a0afde4fc230d26f8faae91decb52b
TJ-Abbing <tjabbing@gmail.com>
2022-07-20 16:06:53 +0200

    Changed README.md and changed fish colors

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Game mechanic
 In dit prototype onderzoek ik de game mechanic 'collision'.
-## Next prototype
-None
-## Previous prototype
+## Prototype 1
+https://github.com/TJ-Abbing/PRG04-2021-2022-prototype-1
+## Prototype 2
 https://github.com/TJ-Abbing/PRG04-2021-2022-prototype-2
+## Prototype 3
+https://github.com/TJ-Abbing/PRG04-2021-2022-prototype-3
 ## Final game
 https://github.com/TJ-Abbing/PRG04-2021-2022-eindopracht
--- src/fish.ts
+++ src/fish.ts
@@ -1,5 +1,4 @@
 import * as PIXI from 'pixi.js'
-
 import { Game } from './game';
 export class Fish extends PIXI.Sprite{
     private game: Game;
@@ -8,12 +7,15 @@ export class Fish extends PIXI.Sprite{
         this.game = game;
         this.anchor.set(0.5);
         this.y = 100;
+        this.tint = Math.random() * 0xFF0000;
         this.x = Math.random() * 800;
         this.y = Math.random() * 450;
     }
     public update(delta: number){
         this.x -= delta * 10;
         if(this.x < (0 - this.width)){
+            // finish
+            // this.game.finish();
             this.x = 800;
             this.y = Math.random() * 450;
         }
